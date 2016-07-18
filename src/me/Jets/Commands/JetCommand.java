1fb4b28dffff1bb85fb7da2f70223834ad7861cb
David Jacquish <kidjac919@mac.com>
2016-07-18 13:10:04 -0400

    awd

--- src/me/Jets/Commands/JetCommand.java
+++ src/me/Jets/Commands/JetCommand.java
@@ -1,7 +1,7 @@
 package me.Jets.Commands;
 
 public class JetCommand extends CommandBase {
-
+	
 	public JetCommand(AccessType accessType) {
 		super(accessType);
 		// TODO Auto-generated constructor stub
