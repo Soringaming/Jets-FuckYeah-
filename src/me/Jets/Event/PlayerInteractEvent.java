db99eec57448afa0d84e8d1ec57b9b2d84fb1e30
David Jacquish <kidjac919@mac.com>
2016-07-19 04:07:01 -0400

    Fully fixed I think

--- src/me/Jets/Event/PlayerInteractEvent.java
+++ src/me/Jets/Event/PlayerInteractEvent.java
@@ -21,44 +21,50 @@ public class PlayerInteractEvent implements Listener {
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onInteract(
 			final org.bukkit.event.player.PlayerInteractEvent event) {
-		
+
 		Player player = event.getPlayer();
-		
+
 		if (!jets.containsKey(player))
 			return;
-		
+
 		Action action = event.getAction();
-		
-		
-		
+
 		ItemStack item = event.getItem();
-		
+
 		if (item.getType().equals(Material.STONE_BUTTON)) {
-			player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_ATTACK_CRIT, 1, 10);
-			
+			player.playSound(player.getLocation(),
+					Sound.ENTITY_PLAYER_ATTACK_CRIT, 1, 10);
+
 			int dist = 32;
 			Location loc = player.getEyeLocation();
-			
-			Item bullet = player.getWorld().dropItem(loc, new ItemStack(Material.STONE_BUTTON, 1));
+
+			Item bullet = player.getWorld().dropItem(loc,
+					new ItemStack(Material.STONE_BUTTON, 1));
 			bullet.setGravity(false);
 			bullet.setInvulnerable(true);
 			bullet.setPickupDelay(1000000);
+			boolean hit = false;
 			for (int i = 0; i < dist * 4; i++) {
+				player.sendMessage("dist: " + ((double) i / 4d));
 				loc.add(player.getEyeLocation().getDirection().multiply(.25));
 				bullet.teleport(loc);
 				if (bullet.getWorld().getBlockAt(loc).getType().isSolid()) {
-					bullet.remove();
 					break;
 				}
 				for (Entity e : bullet.getNearbyEntities(1, 1, 1)) {
-					if (e instanceof LivingEntity && !e.equals(player) && !e.equals(bullet)) {
+					if (e instanceof LivingEntity && !e.equals(player)
+							&& !e.equals(bullet)) {
 						LivingEntity le = (LivingEntity) e;
-						le.damage(4, player);
-						bullet.remove();
+						le.damage(8, player);
+						le.setNoDamageTicks(0);
+						hit = true;
 						break;
 					}
 				}
+				if (hit)
+					break;
 			}
+			bullet.remove();
 		}
 	}
 }
